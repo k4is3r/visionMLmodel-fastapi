89e38cc75b005a2792024fb0bcecc3650083f79f
k4is3r <ejwimery@gmail.com>
2022-02-24 20:31:20 -0600

    adding health_check route on the router

--- app/routers.py
+++ app/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from endpoints import home_page
+from endpoints import home_page, health_check
 
 router = APIRouter()
 
 router.include_router(home_page.router, tags=["Home page"])
+router.include_router(health_check.router, tags=["Health Check"])
 
